c6f2891d1628e2cd84a491158c9343f104d66a1d
Junior Ayala <jayala@pdval.gob.ve>
2018-01-08 15:47:41 -0400

    archivos ejemplos de config para tener de donde copiar

---
+++ sample.general.config.inc.php
@@ -0,0 +1,26 @@
+<?php
+error_reporting(0);
+define('DB_USUARIO','root', true);
+define('DB_CLAVE', 'root', true);
+define('DB_HOST', 'localhost', true);
+define('DB_SELECTRA_CONF', 'selectra_conf_pyme_cealco', true);#sisalud_selectraconf
+define('DB_SELECTRA_CONT', 'pyme_contabilidad', true);
+define('DB_SELECTRA_NOM', 'pyme_nomina', true);
+define('DB_SELECTRA_FAC', 'pyme_prueba_standar_cealco', true);
+define('SUGARCRM', 'sugarcrm',true);
+/* * CONSTANTES UTILIZADAS POR LA INTERFAZ DE REGISTRO DE EVENTOS (LOG) */
+define('REG_INFO',0, true);
+define('REG_LOGIN_OK',1, true);
+define('REG_LOGIN_FAIL',2, true);
+define('REG_LOGOUT',3, true);
+define('REG_SESSION_INVALIDATE',4, true);
+define('REG_SESSION_READ_ERROR',5, true);
+define('REG_SQL_OK',6, true);
+define('REG_SQL_FAIL',7, true);
+define('REG_ILLEGAL_ACCESS',8, true);
+define('REG_ALL',9, true);
+/** * $config es un "por ahora", mientras se define donde va a residir la configuracion general de selectra **/
+ $config['bd']='mysql';
+ /** Archivo _temporal_ para ir colocando las funciones generales... :/ */
+ require_once('funciones.inc.php');
+?>
